14cf00158d0686b6aeb16ceeb0747abb56a3eb51
Chloe Le <yenchi.le1199@gmail.com>
2022-10-27 10:32:11 -0700

    Add function to clear Test3
    Co-authored-by: Brian Noh <noh24@ymail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -29,3 +29,24 @@ function cardValidator(num) {
     return parseInt(num);
   });
 }
+
+// Test3:  Double second number 
+// Code: 
+// num = "35"
+// cardValidator();
+// Expected Output: [3, 10]; 
+
+const num = "35";
+
+function cardValidator(num) {
+  const numArray = num.split("");
+  let newArray = numArray.map(Number);
+  newArray[1] *= 2;
+  return newArray;
+}
+
+// Test4:  If second number is equal or greater than 10, add 1st index to 2nd index.
+// Code: 
+// num = "35"
+// cardValidator();
+// Expected Output: [3, 1]; 
