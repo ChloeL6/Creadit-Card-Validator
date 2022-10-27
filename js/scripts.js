bff2e3a210987bae8dd1cd7e14db75f1d840d4a6
Chloe Le <yenchi.le1199@gmail.com>
2022-10-27 10:23:11 -0700

    Add function to clear Test2
    Co-authored-by: Brian Noh <noh24@ymail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,24 @@ const num = "35";
 
 function cardValidator(num) {
   return num.split("");
-
 }
 
+// Test2:  Convert strings to numbers
+// Code: 
+// num = "35"
+// cardValidator();
+// Expected Output: [3, 5]; 
 
+const num = "35";
+
+function cardValidator(num) {
+  const numArray = num.split("");
+  return numArray.map(Number);
+}
+
+function cardValidator(num) {
+  const numArray = num.split("");
+  return numArray.map(function(num) {
+    return parseInt(num);
+  });
+}
