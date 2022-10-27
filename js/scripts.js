7790f9e23170f3c52be7fd94b6023bce9ed1a92c
Chloe Le <yenchi.le1199@gmail.com>
2022-10-27 10:12:08 -0700

    Add function to clear Test1
    Co-authored-by: Brian Noh <noh24@ymail.com>

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// Test1: Split numbers into array
+// Code: 
+// num = "35"
+// cardValidator();
+// Expected Output: ['3', '5']; 
+
+const num = "35";
+
+function cardValidator(num) {
+  return num.split("");
+
+}
+
+
